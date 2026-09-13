a1bac19f264bc11b1fe724d1da575842aca02c6c
Rittik Gupta <rittikgupta07professional@gmail.com>
2026-09-13 17:00:50 +0530

    feat: Collections Section Added

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../Components/Layout/Hero'
+import GenderCollectionSection from '../Components/Products/GenderCollectionSection'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
+        <GenderCollectionSection/>
     </div>
   )
 }
