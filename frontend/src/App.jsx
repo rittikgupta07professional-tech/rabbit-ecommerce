0cc12c04dab66189d14c2391495342f282ace60c
Rittik Gupta <rittikgupta07professional@gmail.com>
2026-09-13 14:40:04 +0530

    feat: created Navbar with Search and Cart Drawer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import UserLayout from './Components/Layout/UserLayout'
 
 const App = () => {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<UserLayout/>}>
+      {/* User Layout */}
+    </Route>
+    <Route>
+       {/* Admin Layout */}
+    </Route>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
---
+++ frontend/src/Components/Common/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+import {HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight} from 'react-icons/hi2'
+import SearchBar from './SearchBar'
+import CartDrawer from '../Layout/CartDrawer'
+
+const Navbar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleCartDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  return (
+    <>
+      <nav className='container mx-auto flex item-center justify-between py-4 px-6'>
+        {/* Left Logo */}
+        <div className="">
+            <Link to='/' className='text-2xl font-medium'>Rabbit</Link>
+        </div>
+        {/* Center - Navigation Links */}
+        <div className="hidden md:flex space-x-6">
+            <Link to='' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Men</Link>
+            <Link to='' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Women</Link>
+            <Link to='' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Top Wear</Link>
+            <Link to='' className='text-gray-700 hover:text-black text-sm font-medium uppercase'>Bottom Wear</Link>
+        </div>
+        {/* Right - Icon Section */}
+        <div className="flex item-center space-x-4">
+            <Link to='/profile' className='hover:text-black'>
+            <HiOutlineUser className='h-6 w-6 text-gray-700'/>
+            </Link>
+            <button onClick={toggleCartDrawer()} className='relative hover:text-black'>
+                <HiOutlineShoppingBag className='h-6 w-6 -mt-1.5 text-gray-700'/>
+                <span className='absolute -top-1 bg-rabbit-red text-white text-xs rounded-full px-2 py-0.5'>4</span>
+            </button>
+            {/* Search Icon  */}
+            <div className="overflow-hidden">
+                <SearchBar/>
+            </div>
+
+            <button className='md:hidden'>
+                <HiBars3BottomRight className='h-6 w-6 text-gray-700'/>
+            </button>
+        </div>
+      </nav>
+      <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}/>
+    </>
+  )
+}
+
+export default Navbar
