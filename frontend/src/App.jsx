1ee38408188099305b1aa434bfb4b4a9a5fb51ca
Rittik Gupta <rittikgupta07professional@gmail.com>
2026-09-18 01:46:55 +0530

    Best Seller UI and Funtionality Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {Toaster} from 'sonner'
 import UserLayout from './Components/Layout/UserLayout'
 import Home from './Pages/Home'
 
 const App = () => {
   return (
    <BrowserRouter>
+   <Toaster position="top-right"/>
    <Routes>
     <Route path='/' element={<UserLayout/>}>
       {/* User Layout */}
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Layout/Hero'
 import GenderCollectionSection from '../Components/Products/GenderCollectionSection'
 import NewArrival from '../Components/Products/NewArrival'
+import ProductDetails from '../Components/Products/ProductDetails'
 
 const Home = () => {
   return (
@@ -9,6 +10,9 @@ const Home = () => {
         <Hero/>
         <GenderCollectionSection/>
         <NewArrival/>
+        {/* Best Seller */}
+        <h2 className='text-3xl text-center font-bold mb-4 mt-6'>Best Seller</h2>
+        <ProductDetails/>
     </div>
   )
 }
