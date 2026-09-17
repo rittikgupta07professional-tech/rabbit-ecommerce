cac16a5f4c58bb253c1331635146721eeb30cef3
Rittik Gupta <rittikgupta07professional@gmail.com>
2026-09-18 02:02:09 +0530

    You Also May Like Section Done with Functionality

--- frontend/src/Components/Products/ProductDetails.jsx
+++ frontend/src/Components/Products/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
+import ProductGrid from "./ProductGrid";
 
 const selectedProduct = {
   name: "Stylish Jacket",
@@ -21,6 +22,49 @@ const selectedProduct = {
     },
   ],
 };
+
+const similarProducts = [
+    {
+        _id: 1,
+        name: "Product 1",
+        price: 100,
+        images: [
+            {
+                url: "https://picsum.photos/500/500?random=4",
+            },
+        ]
+    },
+    {
+        _id: 2,
+        name: "Product 2",
+        price: 100,
+        images: [
+            {
+                url: "https://picsum.photos/500/500?random=7",
+            },
+        ]
+    },
+    {
+        _id: 3,
+        name: "Product 3",
+        price: 100,
+        images: [
+            {
+                url: "https://picsum.photos/500/500?random=6",
+            },
+        ]
+    },
+    {
+        _id: 4,
+        name: "Product 4",
+        price: 100,
+        images: [
+            {
+                url: "https://picsum.photos/500/500?random=9",
+            },
+        ]
+    },
+]
 
 const ProductDetails = () => {
 
@@ -171,7 +215,12 @@ const ProductDetails = () => {
               </div>
           </div>
         </div>
-        
+        <div className="mt-20">
+            <h2 className="text-2xl text-center font-medium mb-4">
+                You May Also Like
+            </h2>
+            <ProductGrid products={similarProducts}/>
+        </div>
       </div>
     </div>
   );
