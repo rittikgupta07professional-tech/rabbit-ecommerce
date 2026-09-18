1f8c1fae6cb4abc10243ed31f2517a894db48924
Rittik Gupta <rittikgupta07professional@gmail.com>
2026-09-19 01:36:27 +0530

    feat: Login and Register Page Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import {Toaster} from 'sonner'
 import UserLayout from './Components/Layout/UserLayout'
 import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
 
 const App = () => {
   return (
@@ -12,7 +14,8 @@ const App = () => {
     <Route path='/' element={<UserLayout/>}>
       {/* User Layout */}
       <Route index element={<Home/>} />
-
+      <Route path='/login' element={<Login/>} />
+      <Route path='/register' element={<Register/>} />
     </Route>
     <Route>
        {/* Admin Layout */}
